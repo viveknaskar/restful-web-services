ea16955736df60a225f0594bbc001c93b1f7e32d
Vivek Naskar <viveknaskar2912@gmail.com>
2020-11-23 18:23:35 +0530

    Renamed the main class

--- src/main/java/com/viveknaskar/restfulwebservices/SocialMediaRestfulWebServicesApplication.java
+++ src/main/java/com/viveknaskar/restfulwebservices/SocialMediaRestfulWebServicesApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestfulWebServicesApplication {
+public class SocialMediaRestfulWebServicesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestfulWebServicesApplication.class, args);
+		SpringApplication.run(SocialMediaRestfulWebServicesApplication.class, args);
 	}
 
 }
