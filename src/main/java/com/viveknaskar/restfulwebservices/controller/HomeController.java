ef8f69226f76793c27ef9cdaf6df44f308f30443
Vivek Naskar <viveknaskar2912@gmail.com>
2020-11-25 02:39:33 +0530

    Optimized & improved the internationalisation implementation after removing a Bean and fetching the messages.basename from application.properties

--- src/main/java/com/viveknaskar/restfulwebservices/controller/HomeController.java
+++ src/main/java/com/viveknaskar/restfulwebservices/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.viveknaskar.restfulwebservices.controller;
 import com.viveknaskar.restfulwebservices.beans.WelcomeDeveloper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -32,7 +33,7 @@ public class HomeController {
     }
 
     @GetMapping(path = "/hello-developer-internationalized")
-    public String helloDeveloperInternationalisation(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
-        return messageSource.getMessage("morning.greetings", null, locale);
+    public String helloDeveloperInternationalisation() {
+        return messageSource.getMessage("morning.greetings", null, LocaleContextHolder.getLocale());
     }
 }
