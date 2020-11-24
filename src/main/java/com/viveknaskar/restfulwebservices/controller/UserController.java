e48f212838721b714a40d3124d35499917bafa3d
Vivek Naskar <viveknaskar2912@gmail.com>
2020-11-25 02:00:48 +0530

    Implemented HATEOAS for RESTful services

--- src/main/java/com/viveknaskar/restfulwebservices/controller/UserController.java
+++ src/main/java/com/viveknaskar/restfulwebservices/controller/UserController.java
@@ -4,6 +4,8 @@ import com.viveknaskar.restfulwebservices.beans.User;
 import com.viveknaskar.restfulwebservices.exceptions.UserNotFoundException;
 import com.viveknaskar.restfulwebservices.services.UserDaoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -11,6 +13,9 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class UserController {
@@ -24,12 +29,26 @@ public class UserController {
     }
 
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id) {
+    public EntityModel<User> retrieveUser(@PathVariable int id) {
         User user = userDaoService.findOne(id);
         if (user==null) {
             throw new UserNotFoundException("id: " + id);
         }
-        return userDaoService.findOne(id);
+
+        /**
+         * HATEOAS
+         * "all-users", SERVER_PATH + "/users"
+         * retrieveAllUsers
+         */
+        EntityModel<User> resource = EntityModel.of(user);
+
+        WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+
+        // Returning a set of links
+        resource.add(linkTo.withRel("all-users"));
+
+        return resource;
+
     }
 
     @PostMapping("/users")
