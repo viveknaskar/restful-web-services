b833508e67ab3c6e47877d2e9f237a75020e025d
Vivek Naskar <viveknaskar2912@gmail.com>
2020-11-29 03:21:35 +0530

    Enhanced the Swagger documentation describing some of the sections making it more understandable

--- src/main/java/com/viveknaskar/restfulwebservices/SwaggerConfig.java
+++ src/main/java/com/viveknaskar/restfulwebservices/SwaggerConfig.java
@@ -2,16 +2,35 @@ package com.viveknaskar.restfulwebservices;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
+    public static final Contact DEFAULT_CONTACT = new Contact("Vivek", "https://thedeveloperstory.com", "helloworld@example.com");
+
+    public static final ApiInfo DEFAULT_API_INFO = new ApiInfo("Social Media REST API",
+            "Social Media REST API Documentation", "1.0", "urn:tos", DEFAULT_CONTACT.getName(),
+            "Apache 2.0", "https://www.apache.org/licenses/LICENSE-2.0");
+
+    public static final Set<String> DEFAULT_PRODUCES_CONSUMES =
+            new HashSet<>(Arrays.asList("application/json", "application/xml"));
+
+
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2);
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(DEFAULT_API_INFO)
+                .produces(DEFAULT_PRODUCES_CONSUMES)
+                .consumes(DEFAULT_PRODUCES_CONSUMES);
     }
 }
--- src/main/java/com/viveknaskar/restfulwebservices/beans/User.java
+++ src/main/java/com/viveknaskar/restfulwebservices/beans/User.java
@@ -1,17 +1,27 @@
 package com.viveknaskar.restfulwebservices.beans;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+/**
+ * @ApiModel and @ApiModelProperty are the annotations for Swagger documentation
+ * for describing the Model class and properties
+ */
+@ApiModel(description = "All the descriptions about Users")
 public class User {
 
     private Integer id;
 
     @Size(min = 2, message = "Name should have at least 2 characters")
+    @ApiModelProperty(notes = "Name should at least have 2 characters")
     private String name;
 
     @Past
+    @ApiModelProperty(notes = "Date of Birth cannot be a past date")
     private Date dateOfBirth;
 
     public User(Integer id, String name, Date dateOfBirth) {
