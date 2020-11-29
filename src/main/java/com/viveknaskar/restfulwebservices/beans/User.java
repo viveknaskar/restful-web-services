4298d914b6991508a84f61d9cd89bf9b83faf4e6
Vivek Naskar <viveknaskar2912@gmail.com>
2020-11-29 23:10:50 +0530

    Created User entity with some test data

--- src/main/java/com/viveknaskar/restfulwebservices/beans/User.java
+++ src/main/java/com/viveknaskar/restfulwebservices/beans/User.java
@@ -3,6 +3,9 @@ package com.viveknaskar.restfulwebservices.beans;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
@@ -12,8 +15,11 @@ import java.util.Date;
  * for describing the Model class and properties
  */
 @ApiModel(description = "All the descriptions about Users")
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue
     private Integer id;
 
     @Size(min = 2, message = "Name should have at least 2 characters")
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user values(1, sysdate(), 'James');
+insert into user values(2, sysdate(), 'Stephen');
+insert into user values(3, sysdate(), 'Martin');
